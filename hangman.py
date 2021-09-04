0d837c146529b521fbe1dbf15d171a99e990ec8c
Dane Chase <danechase@gmail.com>
2021-09-03 20:03:50 -0500

    title screen and game over screen

--- hangman.py
+++ hangman.py
@@ -1,8 +1,8 @@
 #pylint: disable=missing-module-docstring
-#pylint:disable=invalid-name
-#pylint:disable=global-statement
-#pylint:disable=unused-variable
-#pylint:disable=redefined-outer-name
+# pylint:disable=invalid-name
+# pylint:disable=global-statement
+# pylint:disable=unused-variable
+# pylint:disable=redefined-outer-name
 
 import random
 import os
@@ -12,15 +12,15 @@ from pygame.constants import KEYDOWN
 from words import word_list, descriptions
 
 pygame.init()
-pygame.mixer.music.load(os.path.join('assets','hedwig.mid'))
+pygame.mixer.music.load(os.path.join('assets', 'hedwig.mid'))
 
 
-#music setup
-VOL_NUM=1
-vol_image_rect=(5,5,300,300)
-vol_1=pygame.mixer.music.get_volume()/50
-vol_2=vol_1*10
-vol_3=1
+# music setup
+VOL_NUM = 1
+vol_image_rect = (5, 5, 300, 300)
+vol_1 = pygame.mixer.music.get_volume()/50
+vol_2 = vol_1*10
+vol_3 = 1
 
 pygame.mixer.music.set_volume(vol_1)
 pygame.mixer.music.play()
@@ -35,11 +35,12 @@ pygame.display.set_caption("Harry Potter Hangman!!")
 LETTER_FONT = pygame.font.Font(os.path.join('assets', 'HP.ttf'), 20)
 DESC_FONT = pygame.font.Font(os.path.join('assets', 'HP.ttf'), 35)
 WORD_FONT = pygame.font.Font(os.path.join('assets', 'HP.ttf'), 50)
+TITLE_FONT = pygame.font.Font(os.path.join('assets', 'HP.ttf'), 90)
 
 # game colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (220,20,60)
+RED = (220, 20, 60)
 
 # hangman image loading
 images = []
@@ -48,15 +49,15 @@ for i in range(13, -1, -1):
         'assets', f'hangman{i}.jpg')).convert()
     images.append(image)
 
-#vol images loading
-volume_images=[]
-for i in range(0,4):
-    image=pygame.image.load(os.path.join('assets',f'vol_{i}.jpg')).convert()
+# vol images loading
+volume_images = []
+for i in range(0, 4):
+    image = pygame.image.load(os.path.join('assets', f'vol_{i}.jpg')).convert()
     volume_images.append(image)
 
 # global game vars
-HIGH_SCORE=0
-ATTEMPTS=13
+HIGH_SCORE = 0
+ATTEMPTS = 13
 HANGMAN_STATUS = 0
 WORD_INDEX = 0
 WORDS = ''
@@ -72,20 +73,20 @@ START_Y = 600
 
 def init():
     '''to reset global variables when playing again'''
-    global HANGMAN_STATUS, WORD_INDEX, WORDS, DESCRIPTION, GUESSED,LETTERS
-    LETTERS=[]
+    print(len(descriptions), len(word_list))
+    global HANGMAN_STATUS, WORD_INDEX, WORDS, DESCRIPTION, GUESSED, LETTERS, ATTEMPTS
+    LETTERS = []
     for i in range(26):
         x = START_X + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
         y = START_Y + ((i // 13) * (GAP + RADIUS * 2))
         LETTERS.append([x, y, chr(A+i), True])
-    WORD_INDEX = random.randint(0, len(word_list)-1)
-    WORDS = word_list[WORD_INDEX].upper()
     DESCRIPTION = descriptions[WORD_INDEX]
     GUESSED = [' ']
 
+
 def display_message(message, desc):
     '''prints display message to the screen'''
-    pygame.time.delay(1000)
+    pygame.time.delay(500)
     win.fill(WHITE)
     text = WORD_FONT.render(message, 1, BLACK)
     win.blit(text, (WIDTH/2 - text.get_width() /
@@ -93,29 +94,77 @@ def display_message(message, desc):
     text = DESC_FONT.render(desc, 1, BLACK)
     win.blit(text, (WIDTH/2 - text.get_width()/2, HEIGHT/2+text.get_height()))
     pygame.display.update()
-    pygame.time.delay(6000)
+    pygame.time.delay(2500)
+
 
 def set_volume():
+    '''sets volume of the game'''
     global VOL_NUM
-    if VOL_NUM==3:
-        VOL_NUM=0
+    if VOL_NUM == 3:
+        VOL_NUM = 0
     else:
-        VOL_NUM+=1
-    if VOL_NUM==0:
+        VOL_NUM += 1
+    if VOL_NUM == 0:
         pygame.mixer.music.set_volume(0)
-    if VOL_NUM==1:
+    if VOL_NUM == 1:
         pygame.mixer.music.set_volume(vol_1)
-    if VOL_NUM==2:
+    if VOL_NUM == 2:
         pygame.mixer.music.set_volume(vol_2)
-    if VOL_NUM==3:
+    if VOL_NUM == 3:
         pygame.mixer.music.set_volume(vol_3)
-    
 
-    
+
+def title_screen():
+    '''displays title screen'''
+    global win
+    win.fill(WHITE)
+    win.fill(WHITE)
+    text = TITLE_FONT.render('Harry Potter Hangman', 1, BLACK)
+    win.blit(text, (WIDTH/2 - text.get_width() /
+             2, HEIGHT/2 - text.get_height()/2))
+    text = DESC_FONT.render(
+        'See how many words you can guess before the deed is done.', 1, BLACK)
+    win.blit(text, (WIDTH/2 - text.get_width() /
+             2, HEIGHT/2 + 2*text.get_height()))
+    text = DESC_FONT.render(
+        "Press 'Enter' to begin!!!", 1, RED)
+    win.blit(text, (WIDTH/2 - text.get_width() /
+             2, HEIGHT/2 + 4*text.get_height()))
+    pygame.display.update()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == KEYDOWN and event.key == 13:
+                return
+
+
+def gameOver():
+    '''game over functionality'''
+    global HANGMAN_STATUS, HIGH_SCORE, ATTEMPTS
+    print(HANGMAN_STATUS)
+    display_message(
+        f"Game Over, you achieved a high score of {HIGH_SCORE}!", "Play again? (Y/N)")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+            if event.type == KEYDOWN and event.key >= 97 and event.key <= 122:
+                ltr = chr(event.key).upper()
+                if ltr == 'N':
+                    return False
+                elif ltr == 'Y':
+                    HANGMAN_STATUS = 0
+                    ATTEMPTS = 13
+                    HIGH_SCORE = 0
+                    WORD_INDEX = random.randint(0, len(word_list)-1)
+                    WORDS = word_list[WORD_INDEX].upper()
+                    return True
+
+
 def draw_volume():
-    global vol_image_rect
-    
-    win.blit(volume_images[VOL_NUM],(5,5))
+    '''draws volume indicator'''
+    win.blit(volume_images[VOL_NUM], (5, 5))
     pygame.display.update()
 
 
@@ -123,9 +172,9 @@ def display():
     '''drawing the assets'''
     win.fill(WHITE)
 
-    #High Score
+    # High Score
     text = WORD_FONT.render(f'High Score: {HIGH_SCORE}', 1, BLACK)
-    win.blit(text,(800,40))
+    win.blit(text, (800, 40))
 
     # buttons with LETTERS
     for letter in LETTERS:
@@ -145,12 +194,12 @@ def display():
 
     text = WORD_FONT.render(display_word, 1, BLACK)
     win.blit(text, (425, 250))
-    #hangman image
+    # hangman image
     win.blit(images[HANGMAN_STATUS], (50, 200))
 
-    #attempts left
-    text=LETTER_FONT.render(f'Remaining Attempts: {ATTEMPTS}',1,RED)
-    win.blit(text,(120,470))
+    # attempts left
+    text = LETTER_FONT.render(f'Remaining Attempts: {ATTEMPTS}', 1, RED)
+    win.blit(text, (120, 470))
 
     draw_volume()
     pygame.display.update()
@@ -158,7 +207,6 @@ def display():
 
 def main():
     """game loop"""
-    init()
     global HANGMAN_STATUS, HIGH_SCORE, ATTEMPTS, GUESSED
     FPS = 60
     clock = pygame.time.Clock()
@@ -169,10 +217,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 RUN = False
-                return RUN
+                return False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
-                if 5<mouse_x<50 and 5<mouse_y<50:
+                if 5 < mouse_x < 50 and 5 < mouse_y < 50:
                     set_volume()
                 for letter in LETTERS:
                     x, y, ltr, vis = letter
@@ -185,14 +233,12 @@ def main():
                             test = True
                         if not test:
                             HANGMAN_STATUS += 1
-                            ATTEMPTS-=1
-
-            if event.type==KEYDOWN:
-                print(event.key)
-                if event.key >=97 and event.key <=122:
-                    ltr=chr(event.key).upper()
+                            ATTEMPTS -= 1
+            if event.type == KEYDOWN:
+                if event.key >= 97 and event.key <= 122:
+                    ltr = chr(event.key).upper()
                     for letter in LETTERS:
-                        if ltr not in GUESSED and letter[2]==ltr:
+                        if ltr not in GUESSED and letter[2] == ltr:
                             letter[3] = False
                             test = False
                             GUESSED.append(ltr)
@@ -200,9 +246,8 @@ def main():
                                 test = True
                             if not test:
                                 HANGMAN_STATUS += 1
-                                ATTEMPTS-=1
+                                ATTEMPTS -= 1
                                 display()
-                
 
         won = True
         for letter in WORDS:
@@ -212,17 +257,28 @@ def main():
             display()
             pygame.time.delay(500)
             display_message(f"You WON! The word was:  {WORDS}", DESCRIPTION)
-            RUN=False
-            HIGH_SCORE+=1
+            RUN = False
+            HIGH_SCORE += 1
 
         if HANGMAN_STATUS == 13:
             display()
             pygame.time.delay(500)
             display_message(f"You lost, the word was:  {WORDS}", DESCRIPTION)
             return False
+    word_list.pop(WORD_INDEX)
+    descriptions.pop(WORD_INDEX)
     return True
 
+
+title_screen()
+init()
 while True:
+    WORD_INDEX = random.randint(0, len(word_list)-1)
+    WORDS = word_list[WORD_INDEX].upper()
+    init()
     if not main():
-        break
+        if gameOver():
+            continue
+        else:
+            break
 pygame.quit()
--- words.py
+++ words.py
@@ -1,5 +1,7 @@
+
+#pylint: disable=line-too-long
 '''word list for the game'''
-word_list=[
+word_list = [
     'Accio',
     'Acid Pops',
     'Alchemy',
@@ -10,9 +12,13 @@ word_list=[
     'Auror',
     'Avada Kedavra',
     'Azkaban',
-    'Goblet of Fire'
-    ]
-descriptions=[
+    'Goblet of Fire',
+    'Butterbeer',
+    'Gringotts',
+    'Buckbeak',
+    'Hippogriff'
+]
+descriptions = [
     'A summoning charm',
     'Candy bought at Honeydukes that burns a hole in your tongue.',
     'A science based on turning metal into pure gold.',
@@ -23,5 +29,9 @@ descriptions=[
     "A wizard charged with capturing death eaters.",
     "The killing curse, one of the three unforgivable curses.",
     "Wizard prison gaurded by Dementors",
-    "Goblet students put their name into to attempt to enter the Triwizard tournament"
-    ]
+    "Goblet students put their name into to attempt to enter the Triwizard tournament",
+    'Butterbeer is a popular wizarding beverage described as tasting "a little bit like less-sickly butterscotch."',
+    "The wizard bank.",
+    'A hippogriff that saved Sirius from the dementors at Hogwarts.',
+    'A magical beast with front legs, wings, and head of an eagle and the body of a horse.'
+]
